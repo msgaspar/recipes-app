2877f771acd603d84dba9e1dfaad350981584f58
Vitor <vitorguima@gmail.com>
2021-07-29 18:22:46 -0300

    requisito 13 finalizado

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <Routes />
+    <>
+      <header>HEADER</header>
+      <SearchBar />
+      <Routes />
+    </>
   );
 }
 
