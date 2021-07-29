d61b521d15f24e3d8e3d938d90019ac6bca72e94
gab-boff <gmeninboff@gmail.com>
2021-07-29 18:21:48 -0300

    Adicionando requisitos 2, 3 e 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,28 @@ import React from 'react';
 export default function Login() {
   return (
     <div>
-      login
+      <label htmlFor="email">
+        E-mail:
+        <input
+          type="email"
+          name="email"
+          data-testid="email-input"
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
     </div>
   );
 }
