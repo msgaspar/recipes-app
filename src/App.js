b63df09d1fcf27becc274fe52a0b48ce3ae81d53
Vitor <vitorguima@gmail.com>
2021-07-29 18:28:49 -0300

    requisito 13 finalizado

--- src/App.js
+++ src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes';
-import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <>
-      <header>HEADER</header>
-      <SearchBar />
-      <Routes />
-    </>
+    <Routes />
   );
 }
 
--- src/pages/FoodRecipes.js
+++ src/pages/FoodRecipes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
 
 export default function FoodRecipes() {
   return (
     <div>
       <Header title="Comidas" />
+      <SearchBar />
       food recipes
     </div>
   );
