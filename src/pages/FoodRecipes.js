585682f81121982ee890ce1197942c2915afa250
Matheus Gaspar <msouzagaspar@gmail.com>
2021-07-30 02:01:12 -0300

    implementação do requisito 12

--- src/pages/FoodRecipes.js
+++ src/pages/FoodRecipes.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
-import SearchBar from '../components/SearchBar';
 
 export default function FoodRecipes() {
   return (
     <div>
       <Header title="Comidas" showSearchIcon />
-      <SearchBar />
       food recipes
     </div>
   );
