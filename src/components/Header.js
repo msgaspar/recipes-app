95638cfecb3ecbf09e1ba69c90b378a913274b5a
Matheus Gaspar <msouzagaspar@gmail.com>
2021-07-30 01:21:12 -0300

    adiciona link para página de perfil

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 
@@ -8,11 +9,13 @@ export default function Header({ title, showSearchIcon }) {
     <header className="container" style={ { backgroundColor: '#E3E3E3' } }>
       <div className="row align-items-center" style={ { height: '60px' } }>
         <div className="col-2">
-          <img
-            data-testid="profile-top-btn"
-            src={ ProfileIcon }
-            alt="link para perfil"
-          />
+          <Link to="/perfil">
+            <img
+              data-testid="profile-top-btn"
+              src={ ProfileIcon }
+              alt="link para perfil"
+            />
+          </Link>
         </div>
         <h1 data-testid="page-title" className="m-0 h4 col-8 text-center">{title}</h1>
         <div className="col-2">
