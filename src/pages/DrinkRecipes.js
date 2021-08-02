3e87f8a2e15b5effa1c3d6d296e96d9358ade7e4
tomsantos07 <xotmsantosx@live.com>
2021-08-02 15:23:31 -0300

    requiisto 20 e 1

--- src/pages/DrinkRecipes.js
+++ src/pages/DrinkRecipes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import LowerMenu from '../components/LowerMenu';
 
 export default function DrinkRecipes() {
   return (
     <div>
       <Header title="Bebidas" showSearchIcon />
       Drink recipes
+      <LowerMenu />
     </div>
   );
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import LowerMenu from '../components/LowerMenu';
 
 export default function Profile() {
   return (
     <div>
       <Header title="Perfil" />
       profile
+      <LowerMenu />
     </div>
   );
 }
