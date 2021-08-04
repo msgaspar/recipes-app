0fe222cde7c3cefeb3bf366716b163b6670f4f6b
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-04 01:11:59 -0300

    finaliza requisitos 48 e 49

--- src/components/IngredientsCheckList.js
+++ src/components/IngredientsCheckList.js
@@ -6,19 +6,26 @@ function IngredientsCheckList({ ingredients }) {
   return (
     <div>
       <h3>Ingredients</h3>
-      <Form className="mt-2 mb-4">
+      <Form className="mt-2 mb-4 ingredients-checklist">
         {
           ingredients.map((name, index) => (
             <div
               key={ index }
-              className="ml-3"
+              className="d-flex align-items-center"
+              data-testid={ `${index}-ingredient-step` }
             >
-              <Form.Check
-                className="d-flex align-items-center"
-                label={ name }
-                data-testid={ `${index}-ingredient-step` }
-                id={ `${index}-ingredient-step` }
+              <input
+                type="checkbox"
+                id={ `${index}-ingredient-step-input` }
+                className="mb-2 ml-4"
               />
+              <label
+                className="ml-3 d-flex align-items-center"
+                htmlFor={ `${index}-ingredient-step-input` }
+                id={ `${index}-ingredient-step` }
+              >
+                { name }
+              </label>
             </div>
           ))
         }
