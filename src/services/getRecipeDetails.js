cc162f90cb33aeba273cc1d9712a1f7139a5a6ed
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-05 23:44:09 -0300

    finaliza requisito 51

--- src/services/getRecipeDetails.js
+++ src/services/getRecipeDetails.js
@@ -1,6 +1,14 @@
-export default async function getRecipeDetails(id) {
+export async function getFoodRecipeDetails(id) {
   const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
   const response = await fetch(URL);
   const data = await response.json();
   return data.meals[0];
+}
+
+export async function getDrinkRecipeDetails(id) {
+  const URL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+  return data.drinks[0];
 }
