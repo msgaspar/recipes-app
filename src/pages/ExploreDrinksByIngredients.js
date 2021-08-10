73023932d02201dcdd3996ed27acbb3c5eba8027
gab-boff <gmeninboff@gmail.com>
2021-08-10 13:37:31 -0300

    Adicionando requisitos 75 e 76

--- src/pages/ExploreDrinksByIngredients.js
+++ src/pages/ExploreDrinksByIngredients.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import LowerMenu from '../components/LowerMenu';
+import Header from '../components/Header';
+import { fetchIngredientsDrink } from '../services/drinkSearch';
 
 export default function ExploreDrinksByIngredients() {
+  const history = useHistory();
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    const func = async () => {
+      const fun = await fetchIngredientsDrink();
+      const ingredientName = Object.values(fun.drinks);
+      const twelve = 12;
+      setIngredients(ingredientName.slice(0, twelve));
+    };
+    func();
+  }, []);
+
   return (
     <div>
       <Header title="Explorar Ingredientes" />
-      explore drink by ingredients
+      <div className="div-cards-ingredients">
+        {ingredients.length > 0 && ingredients.map((ingredient, index) => (
+          <div
+            role="presentation"
+            data-testid={ `${index}-ingredient-card` }
+            key={ ingredient.idIngredient }
+            onClick={ () => history
+              .push({ pathname: '/bebidas' }) }
+          >
+            <img
+              data-testid={ `${index}-card-img` }
+              width="100px"
+              src={ `https://www.thecocktaildb.com/images/ingredients/${ingredient.strIngredient1}-Small.png` }
+              alt=""
+            />
+            <p data-testid={ `${index}-card-name` }>{ingredient.strIngredient1}</p>
+          </div>
+        ))}
+      </div>
       <LowerMenu />
     </div>
   );
