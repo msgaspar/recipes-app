3cd4f86bf09d6b21c59dd5985c4d4a1dc46bc80b
tomsantos07 <xotmsantosx@live.com>
2021-08-10 12:46:49 -0300

    reqs 54 ao 59

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -6,31 +6,7 @@ import shareIcon from '../images/shareIcon.svg';
 import '../DoneRecipes.css';
 
 export default function DoneRecipes() {
-  const doneRecipes = [
-    {
-      id: '52771',
-      type: 'comida',
-      area: 'Italian',
-      category: 'Vegetarian',
-      alcoholicOrNot: '',
-      name: 'Spicy Arrabiata Penne',
-      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      doneDate: '23/06/2020',
-      tags: ['Pasta', 'Curry'],
-    },
-    {
-      id: '178319',
-      type: 'bebida',
-      area: '',
-      category: 'Cocktail',
-      alcoholicOrNot: 'Alcoholic',
-      name: 'Aquamarine',
-      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      doneDate: '23/06/2020',
-      tags: [],
-    },
-  ];
-  // const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
   const [recipes, setRecipes] = useState(doneRecipes);
   const [copyText, setCopyText] = useState('');
   const filterRecipesDone = (type) => {
