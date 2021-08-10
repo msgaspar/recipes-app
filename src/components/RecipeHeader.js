703fbabdd3e184049d1609fee21833ffb1318f1b
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-09 22:03:44 -0300

    finaliza requisito 43

--- src/components/RecipeHeader.js
+++ src/components/RecipeHeader.js
@@ -1,41 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import copy from 'clipboard-copy';
 import { Image, Button, Badge, Row, Col, Container } from 'react-bootstrap';
-import ShareIcon from '../images/shareIcon.svg';
 import WhiteHeartIcon from '../images/whiteHeartIcon.svg';
 import BlackHeartIcon from '../images/blackHeartIcon.svg';
+import CopyLinkButton from './CopyLinkButton';
 
 function RecipeHeader({ imgUrl, title, category, isFavorite, toggleFavorite }) {
-  const [linkCopied, setLinkCopied] = useState(false);
-
-  function handleCopyLink() {
-    const url = window.location.href
-      .split('/')
-      .filter((part) => part !== 'in-progress')
-      .join('/');
-
-    copy(url);
-    setLinkCopied(true);
-
-    const TWO_SECONDS = 2000;
-    setTimeout(() => {
-      setLinkCopied(false);
-    }, TWO_SECONDS);
-  }
-
   return (
     <Container as="header" className="p-0">
-      { linkCopied && (
-        <Row className="justify-content-center align-items-center">
-          <p
-            className="m-0 py-2"
-          >
-            Link copiado!
-
-          </p>
-        </Row>
-      )}
       <Row>
         <Col className="p-0">
           <Image
@@ -61,16 +33,7 @@ function RecipeHeader({ imgUrl, title, category, isFavorite, toggleFavorite }) {
           </h5>
         </Col>
         <Col xs={ 4 } className="px-0 d-flex">
-          <Button
-            data-testid="share-btn"
-            variant="link"
-            onClick={ handleCopyLink }
-          >
-            <Image
-              fluid
-              src={ ShareIcon }
-            />
-          </Button>
+          <CopyLinkButton />
           <Button
             variant="link"
             onClick={ toggleFavorite }
