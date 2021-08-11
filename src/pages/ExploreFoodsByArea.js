547ae3a7c8d5999691406fe542e49231feeb9826
gab-boff <gmeninboff@gmail.com>
2021-08-11 14:10:15 -0300

    Adicionando Tela de Explorar por Área

--- src/pages/ExploreFoodsByArea.js
+++ src/pages/ExploreFoodsByArea.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import LowerMenu from '../components/LowerMenu';
+import Header from '../components/Header';
+import { fetchRecipeAllFood,
+  fetchRecipeBySelectedCountry, fetchRecipeCountry } from '../services/foodSearch';
 
-export default function ExploreFoodsByArea() {
+export default function ExploreCountry() {
+  const history = useHistory();
+  const [countries, setCountries] = useState([]);
+  const [countrySelected, setCountrySelected] = useState('All');
+  const [recepies, setRecepies] = useState([]);
+  const twelve = 12;
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const newCountries = await fetchRecipeCountry();
+      setCountries(newCountries.meals);
+    };
+    getCountries();
+  }, []);
+
+  useEffect(() => {
+    let newRecepies;
+    const getRecepies = async () => {
+      if (countrySelected === 'All') {
+        newRecepies = await fetchRecipeAllFood();
+      } else {
+        newRecepies = await fetchRecipeBySelectedCountry(countrySelected);
+      }
+      setRecepies(newRecepies.meals.slice(0, twelve));
+    };
+    getRecepies();
+  }, [countrySelected]);
   return (
     <div>
       <Header title="Explorar Origem" showSearchIcon />
-      explore foods by area
+      <select
+        data-testid="explore-by-area-dropdown"
+        name="country"
+        id="id-country"
+        onChange={ ({ target }) => setCountrySelected(target.value) }
+      >
+        <option key="0" data-testid="All-option">All</option>
+        {countries.map((country, index) => (
+          <option
+            key={ index + 1 }
+            data-testid={ `${country.strArea}-option` }
+          >
+            {country.strArea}
+          </option>
+        ))}
+      </select>
+
+      <div>
+        { recepies.map((element, index) => (
+          <div
+            role="presentation"
+            key={ index }
+            data-testid={ `${index}-recipe-card` }
+            onClick={ () => history.push(`/comidas/${element.idMeal}`) }
+          >
+            <img
+              src={ element.strMealThumb }
+              data-testid={ `${index}-card-img` }
+              alt={ element.strMeal }
+              width="100px"
+            />
+            <p data-testid={ `${index}-card-name` }>
+              { element.strMeal}
+            </p>
+          </div>
+        ))}
+      </div>
       <LowerMenu />
     </div>
   );
--- src/services/foodSearch.js
+++ src/services/foodSearch.js
@@ -30,3 +30,21 @@ export const fetchIngredientsFood = () => (
     .then((res) => res.json())
     .then((response) => response)
 );
+
+export const fetchRecipeAllFood = () => (
+  fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
+    .then((res) => res.json())
+    .then((json) => json)
+);
+
+export const fetchRecipeCountry = () => (
+  fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+    .then((res) => res.json())
+    .then((json) => json)
+);
+
+export const fetchRecipeBySelectedCountry = (country) => (
+  fetch(`${'https://www.themealdb.com/api/json/v1/1/filter.php?a='}${country}`)
+    .then((res) => res.json())
+    .then((json) => json)
+);
