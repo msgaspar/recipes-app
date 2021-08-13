64872a640ba8f6923d41f5e060b2f26f1aaed90c
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-13 01:40:39 -0300

    estilização da tela de explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,32 +1,74 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import LowerMenu from '../components/LowerMenu';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 export default function Explore() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar" />
-      <Link
-        to="/explorar/comidas"
+      <div
+        className="d-flex flex-column align-items-center mx-4"
+        style={ {
+          padding: '80px 0 70px',
+          height: '100vh',
+        } }
       >
         <button
+          className="w-100 my-2
+            d-flex flex-column justify-content-center align-items-center"
+          style={ {
+            border: 'none',
+            borderRadius: '8px',
+            flexGrow: 1,
+            backgroundColor: '#FA8334',
+            color: '#fff',
+            fontWeight: 'bold',
+            fontSize: '24px',
+          } }
           type="button"
           data-testid="explore-food"
+          onClick={ () => history.push('/explorar/comidas') }
         >
+          <img
+            style={ {
+              width: '80px',
+              marginBottom: '22px',
+            } }
+            src={ mealIcon }
+            alt="Explorar bebidas"
+          />
           Explorar Comidas
         </button>
-      </Link>
-      <Link
-        to="/explorar/bebidas"
-      >
         <button
+          className="w-100 my-2 d-flex flex-column justify-content-center align-items-center"
+          style={ {
+            borderRadius: '8px',
+            border: 'none',
+            flexGrow: 1,
+            backgroundColor: '#11a999',
+            color: '#fff',
+            fontWeight: 'bold',
+            fontSize: '24px',
+          } }
           type="button"
           data-testid="explore-drinks"
+          onClick={ () => history.push('/explorar/bebidas') }
         >
+          <img
+            style={ {
+              width: '80px',
+              marginBottom: '22px',
+            } }
+            src={ drinkIcon }
+            alt="Explorar bebidas"
+          />
           Explorar Bebidas
         </button>
-      </Link>
+      </div>
       <LowerMenu />
     </div>
   );
