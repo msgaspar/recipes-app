9a542eff3af451bc9882fc41040f5255d42f878a
Matheus Gaspar <msouzagaspar@gmail.com>
2021-08-18 00:51:37 -0300

    elimina problemas de lint em arquivo DoneRecipes.test

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,9 +1,7 @@
-import { fireEvent } from '@testing-library/dom';
-import React from 'react';
-import DoneRecipes from '../pages/DoneRecipes';
-import renderWithRouter from '../RenderWithRouter';
+// import React from 'react';
+// import DoneRecipes from '../pages/DoneRecipes';
+// import renderWithRouter from '../RenderWithRouter';
 
 describe('tests page DoneRecipes(/receitas-feitas)', () => {
-  const { getByText } = renderWithRouter(<DoneRecipes />);
-
+  // const { getByText } = renderWithRouter(<DoneRecipes />);
 });
